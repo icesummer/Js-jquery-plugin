88ccd4c975d855679d8e902cda646994fe93c2bb
icesummer <965192731@qq.com>
2019-07-22 18:13:00 +0800

    Update page.plugin.js

--- page.plugin.js
+++ page.plugin.js
@@ -1,21 +1,22 @@
 /*
-    page version v.1.0.0
+    PageGo version v.1.0.0
 	By icesummer build in July 17, 2019
 	For more information, please visit http://www.icesummer.top
 	// 请求，组装，计算页码
-	$(this).PageGo({
-			page:page,
-			elem:{searchId:'#search-div1',listId:'#list',footId:'#pagefoot'},
-			title:[],
-			param:param,
-			pageFunc:function(opts,param){
-				//代码
+	var pageOne = $(this).PageGo({
+			page:page,//分页请参数
+			param:param,// 查询参数
+			elem:{searchId:'#search-div1',listId:'#list',footId:'#pagefoot'}, // 查询，数据，页码标签定位
+			titles:[],// 数据表头,重写pageFunc时可不填
+			pageFunc:function(brothers,opts,params){
+				//分页请求+组装页面代码：若样式有改变，此处可重写
 			},
-			pageNavigate:function(opts,data){
-				// 页码导航：默认不重写
+			pageNavigate:function(brothers,opts,data){
+				// 页码导航代码：默认不重写
 			}
-		})();
-	
+		});
+	// click查询：
+	pageOne.checkPage({pageNum:2,pageSize:1},param);
 */
 ;(function($,win,undef){
 	var PageGo=function(bodys,settings,inited){
@@ -33,12 +34,62 @@
 	PageGo.util={
 		pageOne:function(brothers,opts){
 			console.info(brothers.params);
-			var data = opts.pageFunc.call(brothers,opts,brothers.params);
-			brothers.result=data;
+			var pageFunc=opts.pageFunc||PageGo.util.pageFunc;
+			brothers.result = pageFunc.call(brothers,brothers,opts,brothers.params);
+			
 			var pageNumShow=opts.pageNumShow||PageGo.util.pageNumShow;
 			var pageNavigate=opts.pageNavigate||PageGo.util.pageNavigate;
 			pageNumShow.call(this,brothers,opts);
-			pageNavigate.call(this,brothers,opts,data);
+			pageNavigate.call(this,brothers,opts,brothers.result);
+		},
+		pageFunc:function(brothers,opts,params){
+			var dataHtmlId = opts.elem.listId;
+			PageGo.util.noneMsg(brothers,opts,dataHtmlId,"正在获取数据，请稍候…");
+			var result ={};
+			$.ajax({
+				url:opts.url,
+				data:params,
+				type:"post",
+				async:false,
+				dateType:'json',
+				contentType:'application/x-www-form-urlencoded',
+				success:function(data){
+					$(dataHtmlId).html("");
+					result = data.data;
+					if(data.code==1){
+						PageGo.util.buildDataHtml(opts.titles,result,dataHtmlId);
+						addDuoxuanClick();
+					}else{
+						PageGo.util.noneMsg(brothers,opts,dataHtmlId,"未查询到数据~");
+					}
+				},error:function(d){
+					$(dataHtmlId).html("查询出错~");
+				}
+			})
+			return result;
+		},
+		buildDataHtml:function(titles,data,dataHtmlId){
+			for(var i=0;i<data.list.length;i++){
+				var _html='<tr onmouseover="onColor(this)" onmouseout="offColor(this)">';
+				for(var j = 0 ;j<titles.length;j++){
+					if(j==0){
+						_html+='<td><input type="checkbox" name="bike" value="'+data.list[i][titles[j]]+'" class="duoxuan" /></td>';
+					}else{
+						var tname='';
+						var title=titles[j];
+						if(title.indexOf(',')>0){
+							tname+=$.trim(data.list[i][title.split(",")[0]])+'/'
+							tname+=$.trim(data.list[i][title.split(",")[1]])+''
+						}else {tname+=$.trim(data.list[i][title])}
+						_html+='<td>'+tname+'</td>';
+					}
+				}
+				_html+='</tr>';
+				$(dataHtmlId).append(_html);
+			}
+		},
+		noneMsg:function(brothers,opts,dataHtmlId,msg){
+			$(dataHtmlId).html('<tr><td colSpan="6"><center><span style="font-size: 14px;color:red;">&nbsp;&nbsp;&nbsp;&nbsp;'+msg+'</span></center></td></tr>');
 		},
 		pageNumShow:function(brothers,opts){
 			var data = brothers.result;
@@ -75,19 +126,23 @@
 				}
 				_html+='</ul>';
 			$(footId+" .right").html(_html);
+			// 跳转页面
 			$(footId+" .right").find("[name='selectPage']").on('click',function(){
 				brothers.params.pageNum=parseInt(this.id);
 				PageGo.util.pageOne.call(brothers,brothers,brothers.settings);
 			})
+			// 上一页
 			$(footId+" .right").find("[name='upPage']").on('click',function(){
 				brothers.params.pageNum=parseInt(brothers.params.pageNum-1);
 				PageGo.util.pageOne.call(brothers,brothers,brothers.settings);
 			});
+			// 下一页
 			$(footId+" .right").find("[name='nextPage']").on('click',function(){
 				brothers.params.pageNum=parseInt(brothers.params.pageNum+1);
 				PageGo.util.pageOne.call(brothers,brothers,brothers.settings);
 			});
 		},
+		
 		toString:Object.prototype.toString,
 		isEmpty:function(val){
 			return val==="" || val===$.trim(this.attr("tip"));
@@ -97,7 +152,7 @@
 		}
 	}
 	PageGo.defaults={
-			page:{pageNum:1,pageSize:10,param:{},
+			page:{pageNum:1,pageSize:10,param:{},title:[],
 				result:{
 					"list":[],
 					"total":0,
